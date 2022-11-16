325a65cdcee0b3adb36fab61ca303a3c2d0e8869
Jake <js19262n@pace.edu>
2022-11-15 21:17:44 -0500

    Lab 8 11/15/2022

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,23 +1,25 @@
 let currentPrice=0, itemCount=0
 
 function addToCart(cookie) {
-    /* 
-        PRICES
-        peanut butter: 20
-        sandies: 30
-        party press: 35
-        chocolate chip: 25
-    */
+    if (cookie == "chocolate chip") {
+        currentPrice = 25 + currentPrice
+    } else if (cookie == "party press") {
+        currentPrice = 35 + currentPrice
+    } else if (cookie == "peanut butter") {
+        currentPrice = 20 + currentPrice
+    } else if (cookie == "sandies") {
+        currentPrice = 30 + currentPrice
+    }
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
-
+    
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
-
+    itemCount++
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+    console.log('You have in your cart ', itemCount, ' items. The grand total is $', currentPrice)
 }
