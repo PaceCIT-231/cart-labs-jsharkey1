061c44c38dfb1aeae3678376c95885169189ff48
Jake <js19262n@pace.edu>
2022-11-21 14:52:18 -0500

    lab9 11/21/22

--- assets/scripts.js
+++ assets/scripts.js
@@ -12,14 +12,36 @@ function addToCart(cookie) {
     }
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
-    
+    let cartItems = document.getElementById('cartItems')
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
     itemCount++
+    cartItems.innerText = itemCount
+
+    let hoverText = document.querySelector('.hoverText')
+    hoverText.innerText = "$" + currentPrice      
+    
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
     console.log('You have in your cart ', itemCount, ' items. The grand total is $', currentPrice)
+    
+    // let text, address;
+    let customer = prompt("Please enter your name:", "Jake Sharkey");
+    let address = prompt("Where can I send these cookies?", "861 Bedford Rd");
+    
+    let hoverText = document.querySelector('.hoverText')
+    hoverText.innerText = "$0", currentPrice=0
+    
+    itemCount=0
+    let cartItems = document.getElementById('cartItems')
+    cartItems.innerText = itemCount
+
 }
+
+function darkMode() {
+	var element = document.body;
+	element.classList.toggle("dark-mode");
+} 
