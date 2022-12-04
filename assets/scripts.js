699c1ea42b2bd43284e36811dcd7526c09fd3e10
Jake <js19262n@pace.edu>
2022-12-04 12:05:31 -0500

    lab10 12/4/2022

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,43 +1,58 @@
-let currentPrice=0, itemCount=0
+const cart = {
+    currentPrice: 0,
+    items: [],
+    addItem: function(cookie, price) {
+        // add a cookie (string) to the items array
+        this.items.push(cookie);
+        //add the price (number) to the currentPrice properties
+        this.currentPrice = price + this.currentPrice;
+    },
+    clear: function() {
+        //reset the currentPrice and items properties
+        this.currentPrice = 0;
+        this.items = [];
+    },
+}
 
 function addToCart(cookie) {
     if (cookie == "chocolate chip") {
-        currentPrice = 25 + currentPrice
+        cart.addItem(cookie, 25)
     } else if (cookie == "party press") {
-        currentPrice = 35 + currentPrice
+        cart.addItem(cookie, 35)
     } else if (cookie == "peanut butter") {
-        currentPrice = 20 + currentPrice
+        cart.addItem(cookie, 20)
     } else if (cookie == "sandies") {
-        currentPrice = 30 + currentPrice
+        cart.addItem(cookie, 30)
     }
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
     let cartItems = document.getElementById('cartItems')
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
-    itemCount++
-    cartItems.innerText = itemCount
+    cartItems.innerText = cart.items.length
 
     let hoverText = document.querySelector('.hoverText')
-    hoverText.innerText = "$" + currentPrice      
+    hoverText.innerText = cart.currentPrice      
     
 }
+
+console.log(cart)
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    console.log('You have in your cart ', itemCount, ' items. The grand total is $', currentPrice)
+    console.log('You have in your cart ', cart.items.length, ' items. The grand total is $', cart.currentPrice)
     
     // let text, address;
     let customer = prompt("Please enter your name:", "Jake Sharkey");
     let address = prompt("Where can I send these cookies?", "861 Bedford Rd");
     
     let hoverText = document.querySelector('.hoverText')
-    hoverText.innerText = "$0", currentPrice=0
+    hoverText.innerText = "$0"
     
-    itemCount=0
+    cart.clear();
     let cartItems = document.getElementById('cartItems')
-    cartItems.innerText = itemCount
+    cartItems.innerText = cart.items.length
 
 }
 
